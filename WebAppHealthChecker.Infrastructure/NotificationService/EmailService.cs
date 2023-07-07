3bb7c76bdbf2f564821feb57000c0cc3083fdf84
هیمن روحانی <hemen.rohani@gmail.com>
2023-07-07 10:23:03 +0330

    Add Email service

--- WebAppHealthChecker.Infrastructure/NotificationService/EmailService.cs
+++ WebAppHealthChecker.Infrastructure/NotificationService/EmailService.cs
@@ -1,4 +1,6 @@
-﻿using WebAppHealthChecker.Application.Common;
+﻿using System.Net.Mail;
+using System.Net;
+using WebAppHealthChecker.Application.Common;
 using WebAppHealthChecker.Application.Common.Interfaces;
 using WebAppHealthChecker.Domain.Entities;
 
@@ -8,6 +10,19 @@ public class EmailService : INotificationService
 {
     public Task SendAsync(string reciver, string text)
     {
-        throw new NotImplementedException();
+        MailMessage mailMessage = new MailMessage();
+        mailMessage.From = new MailAddress("email@mywebsitedomain.com");
+        mailMessage.To.Add(reciver);
+        mailMessage.Subject = "Subject";
+        mailMessage.Body = text;
+
+        SmtpClient smtpClient = new SmtpClient();
+        smtpClient.Host = "smtp.mywebsitedomain.com";
+        smtpClient.Port = 587;
+        smtpClient.UseDefaultCredentials = false;
+        smtpClient.Credentials = new NetworkCredential("username", "password");
+        smtpClient.EnableSsl = true;
+
+        smtpClient.Send(mailMessage);
     }
 }
